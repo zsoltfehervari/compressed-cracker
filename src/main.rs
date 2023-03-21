a308bff497b0833e1fb6dde091d49b995059bcd2
zsoltfehervari <zsoltfehervari@gmx.com>
2023-03-21 15:18:32 +0100

    add: start string

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use std::fs;
 use std::ffi::OsStr;
-//use std::io::BufReader;
+
+//TODO: rar
+// extern crate unrar;
+// use unrar::Archive;
+
+//TODO: pdf
+// use pdf::file::File as PdfFile;
+// use pdf::password::PdfPassword;
 
 use bruteforce::BruteForce;
 use bruteforce::charset::Charset;
 
-// CharacterSet: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~`[]{}|\\:;\"'<>,.?/"
+// symbols: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~`[]{}|\\:;\'<>,.?/"
+
+//extern crate rar;
 
 fn main() {
 
     let args: Vec<String> = std::env::args().skip(1).collect();
 
     if args.len() == 0 {
-        println!("No Archive (.zip,.rar) path specified!");
+        println!("No Archive (.zip, .rar, .pdf) path specified!");
         return;
     };
 
@@ -21,14 +30,15 @@ fn main() {
         return println!("File does not exist!");
     }
 
-    const ALLOWED_EXTENSIONS: [&str; 2] = ["zip","rar"];
+    const ALLOWED_EXTENSIONS: [&str; 3] = ["zip","rar","pdf"];
     let f_extension = f_name.extension().and_then(OsStr::to_str).unwrap();
     if !ALLOWED_EXTENSIONS.contains(&f_extension) {
         return println!("File extension is not allowed!");
     }
 
     const _CHARSET: Charset = Charset::new(&['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '~', '`', '[', ']', '{', '}', '|', '\\', ':', ';', '"','<', '>', ',', '.', '?', '/']);
-    let brute_forcer = BruteForce::new(_CHARSET);
+    let brute_forcer = if args.len() > 1 { BruteForce::new_by_start_string(_CHARSET,args[1].clone())}
+        else { BruteForce::new(_CHARSET) };
 
     let file = fs::File::open(f_name);
 
@@ -53,21 +63,48 @@ fn main() {
         }
 
         // rar
-    }else {
+    }else if  f_extension == ALLOWED_EXTENSIONS[1] {
 
-        // let reader = BufReader::new(file);
-        // let mut archive = rar::Archive::new(reader);
+        // for s in brute_forcer {
+        //
+        //     match Archive::with_password(String::from(&*args[0]),String::from(&s))
+        //     {
+        //         Ok(Archive) => {
+        //             println!("================================================");
+        //             println!("Password found: {}", &s);
+        //             println!("================================================");
+        //             break;
+        //         },
+        //         Err(Archive) => {
+        //             println!("{}", &s);
+        //         },
+        //         _ => {
+        //             println!("{}", &s);
+        //         }
+        //
+        //     }
+        //
+        // }
+
+        // pfd
+    }else{
+
+        // let pdf_file = PdfFile::new(file);
         //
         // for s in brute_forcer {
         //
-        //     if archive.test(s.to_string()) {
+        //     let pdf_password = PdfPassword::from_str(s);
+        //     if pdf_file.check_password(&pdf_password)? {
+        //
         //         println!("================================================");
-        //         println!("Password found: {}", s);
+        //         println!("Password found: {}", &s);
         //         println!("================================================");
         //         break;
-        //     } else {
-        //         println!("{}", s);
+        //
+        //     }else{
+        //         println!("{}", &s);
         //     }
+        //
         //
         // }
 
